52ae9337606b4852c7c53731cc34dc85e418d410
Kristen Burgess <kristen.burgess@doane.edu>
2016-11-30 14:58:57 -0700

    Set up header container and component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import HeaderContainer from '../containers/HeaderContainer.js'
 
 class App extends Component {
   render() {
     return (
-      <div>Hey!</div>
+      <div>
+      <HeaderContainer />
+      </div>
     )
   }
 }
 
-module.exports = App 
+module.exports = App
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import Header from '../components/Header.js'
+
+
+const mapStateToProps = state => {
+  // return an object of redux store data
+  // that you'd like available in your component
+  return {};
+}
+
+const mapDispatchToProps = dispatch => {
+  // return an object of methods you'd like
+  // to dispatch as redux actions
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
