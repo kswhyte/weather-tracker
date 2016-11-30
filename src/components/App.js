142cd415dc56c3499f623f2ba813aa88cb697526
Kristen Burgess <kristen.burgess@doane.edu>
2016-11-30 15:09:46 -0700

    WIP set up geo-location

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import HeaderContainer from '../containers/HeaderContainer.js'
 
 class App extends Component {
+
+  componentWillMount () {
+    console.log('Hello!');
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+    });
+  }
+
   render() {
     return (
       <div>
