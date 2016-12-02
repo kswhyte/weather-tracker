55c134099c027e58c70bdc94f7f6993b5ffa7277
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-01 20:13:55 -0700

    Round current weather temps to nearest integer

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class CurrentWeather extends Component {
   render() {
 
-let currentTemp = Math.round(this.props.temp)
+    const currentTemp = Math.round(this.props.temp)
+
+    const lowTemp = Math.round(this.props.tempMin)
+
+    const highTemp = Math.round(this.props.tempMax)
+
     return (
       <div>
         <h3> Currently: {currentTemp}°F </h3>
-        <h3> Low: {this.props.tempMin}°F </h3>
-        <h3> High: {this.props.tempMax}°F </h3>
+        <h3> Low: {lowTemp}°F </h3>
+        <h3> High: {highTemp}°F </h3>
       </div>
     );
   }
