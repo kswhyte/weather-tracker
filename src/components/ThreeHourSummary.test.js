20732076cd44c1d083cc4519d949d408036e7d1e
Maia Stone <maiadov@gmail.com>
2016-12-03 23:13:23 -0700

    Sets up more test files

---
+++ src/components/ThreeHourSummary.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { expect } from 'chai';
+import ThreeHourSummary from './ThreeHourSummary';
+
+describe('ThreeHourSummary', () => {
+  it('should receive data as props from the Store', () => {
+
+  });
+});
