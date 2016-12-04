1cf700a2fb2e52d8a379c78328f3e26502df13ea
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-04 15:06:21 -0700

    ...

--- src/components/WeekSummary.js
+++ src/components/WeekSummary.js
@@ -9,14 +9,15 @@ const WeekSummary = ({ data }) => {
   const windSpeed = Math.round(data.speed);
   const dateFormatted = moment.unix(data.dt).format('dddd')
   return (
-    <div className="extended-forecast weather-card">
-
-      <h3>{dateFormatted}</h3>
-      <h4>Description: {data.weather[0].description}</h4>
-      <h4>High: {highTemp}°F </h4>
-      <h4>Low: {lowTemp}°F</h4>
-      <h4>Humidity: {humidity}% </h4>
-      <h4>Wind Speed: {windSpeed} MPH</h4>
+    <div className="weather-card">
+      <section className="extended-weather-card">
+        <h3>{dateFormatted}</h3>
+        <h4>Description: {data.weather[0].description}</h4>
+        <h4>High: {highTemp}°F </h4>
+        <h4>Low: {lowTemp}°F</h4>
+        <h4>Humidity: {humidity}% </h4>
+        <h4>Wind Speed: {windSpeed} MPH</h4>
+      </section>
     </div>
   );
 };
