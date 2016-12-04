53328549beb5ca9e1c52935760455a4f60a24a4e
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-03 20:13:10 -0700

    Round numbers and add symbols for extended forecast

--- src/components/WeekSummary.js
+++ src/components/WeekSummary.js
@@ -2,14 +2,20 @@ import React from 'react';
 import moment from 'moment';
 
 const WeekSummary = ({ data }) => {
+
+  let highTemp = Math.round(data.temp.max);
+  let lowTemp = Math.round(data.temp.min);
+  let humidity = Math.round(data.humidity);
+  let windSpeed = Math.round(data.speed);
+
   return (
     <div>
       <h3>{moment.unix(data.dt).calendar()}</h3>
       <h4>Description: {data.weather[0].description}</h4>
-      <h4>High: {data.temp.max}</h4>
-      <h4>Low: {data.temp.min}</h4>
-      <h4>Humidity: {data.humidity}</h4>
-      <h4>Wind Speed: {data.speed}</h4>
+      <h4>High: {highTemp}°F </h4>
+      <h4>Low: {lowTemp}°F</h4>
+      <h4>Humidity: {humidity}% </h4>
+      <h4>Wind Speed: {windSpeed} MPH</h4>
     </div>
   );
 };
