41509a82e226d6b724ee36530c2a6205d15b65fa
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-03 20:06:42 -0700

    Show all necessary data in extended forecast

--- src/components/WeekSummary.js
+++ src/components/WeekSummary.js
@@ -6,6 +6,10 @@ const WeekSummary = ({ data }) => {
     <div>
       <h3>{moment.unix(data.dt).calendar()}</h3>
       <h4>Description: {data.weather[0].description}</h4>
+      <h4>High: {data.temp.max}</h4>
+      <h4>Low: {data.temp.min}</h4>
+      <h4>Humidity: {data.humidity}</h4>
+      <h4>Wind Speed: {data.speed}</h4>
     </div>
   );
 };
